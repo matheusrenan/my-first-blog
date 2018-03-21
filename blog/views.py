91cd1206e01170eb71add6b51452f75215f902a3
matheusrenan <matheusrenan@gmail.com>
2018-03-20 21:50:46 -0300

    Adição de contador de linhas

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,8 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.contadorVisitas+=1
+    post.save()
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
