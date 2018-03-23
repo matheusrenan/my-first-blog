7c8fd82cc3ef080aab9cbef515b9fc7ac5acf6d0
matheusrenan <matheusrenan@gmail.com>
2018-03-23 18:56:54 -0300

    Colocado valor default para o contadorVisitas

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    contadorVisitas = models.PositiveIntegerField()
+    contadorVisitas = models.PositiveIntegerField(default = 0)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
